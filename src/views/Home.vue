705ebe047283fbbcbd6aa21e4e5c849256c20961
lisp105 <lisp105@chinaunicom.cn>
2020-07-21 16:12:33 +0800

    feat: 初始化工程

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,8 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+img {
+  height: pxToRem(100px);
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store
+    oneOfsMap.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          // Provide path to the file with resources
+          resources: ['./src/assets/css/flexible.scss', './src/assets/css/base.scss']
+        }).end()
+      })
+  }
+}
